dcb08c010a313e84a21586fac1365ab18b3762cb
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-09-11 03:12:57 +0800

    tmd 0.1.5

--- Formula/tmd.rb
+++ Formula/tmd.rb
@@ -1,8 +1,8 @@
 class Tmd < Formula
   desc "Modern parser, CLI, and multi-format music rendering toolkit for TMD"
   homepage "https://github.com/zonble/TmdSwift"
-  url "https://github.com/zonble/TmdSwift/archive/refs/tags/0.1.3.tar.gz"
-  sha256 "0a233c115bdc5c66b87868c62c7bc9a854d73b93777c3ec2a30c4a025be67283"
+  url "https://github.com/zonble/TmdSwift/archive/refs/tags/0.1.5.tar.gz"
+  sha256 "9c9d8fc6322661b796842b3d11ccd3a5ef56bfdfa3fe378fda028a1fa94715aa"
   license "MIT"
   head "https://github.com/zonble/TmdSwift.git", branch: "main"
 
